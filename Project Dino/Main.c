f65ebd71d63887b1bf64587b194e0909b719af95
Petru Grozea <petrugrozea@gmail.com>
2017-10-22 01:43:03 +0300

    Obj priting and initialization

--- Project Dino/Main.c
+++ Project Dino/Main.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <Windows.h>
-#include <string.h>
-
-//{ { ".###." },{ "#####" },{ ".###." },{ "#####" },{ ".###." } };
 
 typedef char sprites[5][6];
-
-typedef struct square {
-	char display;
-	int owner;
-}square;
 
 typedef struct object {
 	int owner;
@@ -17,55 +9,61 @@ typedef struct object {
 	sprites sprite;
 }object;
 
-void initialize_the_game_rect(square *);
-void initialize_the_game_object(square *, object);
-void draw_the_image(square[][100]);
+object obj_init(int, int, int, char[][6]);
+void set_console_pos(int, int);
+void draw_the_object(object);
+
+int x, y;
 
 void main() {
 
-	/*HANDLE cons_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	x = 0; y = 0;
+	set_console_pos(x, y);
+
+	object cactus = obj_init(90, 20, 2, " ### ##### ### ##### ### ");
+
+	draw_the_object(cactus);
+
+}
+
+// make a bigger _sprite array and add size parameters
+// or better try to find a way how to pass the array without doing any costly loop
+object obj_init(int _x, int _y, int _owner, char _sprite[26]) {
+	object _obj;
+
+	_obj.x = _x;
+	_obj.y = _y;
+	_obj.owner = _owner;
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			_obj.sprite[i][j] = _sprite[i * 5 + j];
+		}
+	}
+
+	return _obj;
+
+}
+
+void set_console_pos(int _x, int _y) {
+	HANDLE cons_handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD cons_coord;
-	cons_coord.X = 5; cons_coord.Y = 10;
-	SetConsoleCursorPosition(cons_handle, cons_coord);*/
 
-	square game_rect[25][100];
-	object cactus;
+	cons_coord.X = _x; cons_coord.Y = _y;
+	x = _x; y = _y;
 
-	cactus.owner = 2;
-	cactus.x = 90; cactus.y = 20;
-	strcpy_s(cactus.sprite[0], 6, " ### "); strcpy_s(cactus.sprite[1], 6, "#####"); strcpy_s(cactus.sprite[2], 6, " ### "); strcpy_s(cactus.sprite[3], 6, "#####"); strcpy_s(cactus.sprite[4], 6, " ### ");
-
-	initialize_the_game_rect(&game_rect);
-	initialize_the_game_object(&game_rect, cactus);
-	draw_the_image(game_rect);
+	SetConsoleCursorPosition(cons_handle, cons_coord);
 }
 
-void initialize_the_game_rect(square (* _gr)[25][100]) {
-	for (int i = 0; i < 25; i++) {
-		for (int j = 0; j < 100; j++) {
-			(*_gr)[i][j].display = ' ';
-			(*_gr)[i][j].owner = 0;
+void draw_the_object(object _go) {
+
+	for (int i = 0; i < 5; i++) {
+		set_console_pos(_go.x, _go.y + i);
+
+		for (int j = 0; j < 5; j++) {
+			printf("%c", _go.sprite[i][j]);
 		}
 	}
-}
 
-void initialize_the_game_object(square(*_gr)[25][100], object _obj) {
-	for (int i = _obj.y; i < _obj.y + 5; i++) {
-		for (int j = _obj.x; j < _obj.x + 5; j++) {
-			if (_obj.sprite[i - _obj.y][j -_obj.x] != ' ') {
-				(*_gr)[i][j].display = _obj.sprite[(i - _obj.y)][(j - _obj.x)];
-				(*_gr)[i][j].owner = _obj.owner;
-			}
-		}
-	}
-}
-
-void draw_the_image(square _gr[][100]) {
-	for (int i = 0; i < 25; i++) {
-		for (int j = 0; j < 100; j++) {
-			printf("%c", _gr[i][j].display);
-		}
-
-		printf("\n");
-	}
+	set_console_pos(0, y++); y++;
 }
