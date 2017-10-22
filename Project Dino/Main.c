a92c6ece91cd8da2bb0d94f9fa6d5f4c97c0ec23
Petru Grozea <petrugrozea@gmail.com>
2017-10-23 02:10:02 +0300

    Dynamic object initialization + movement test

--- Project Dino/Main.c
+++ Project Dino/Main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <Windows.h>
 
-typedef char sprites[5][6];
+typedef char sprites[10][10];
 
 typedef struct object {
 	int owner;
-	int x, y;
+	int x, y, _lx, _ly;
 	sprites sprite;
 }object;
 
-object obj_init(int, int, int, char[][6]);
+object obj_init(int, int, int, char[], int, int);
 void set_console_pos(int, int);
 void draw_the_object(object);
 
@@ -20,24 +20,38 @@ void main() {
 	x = 0; y = 0;
 	set_console_pos(x, y);
 
-	object cactus = obj_init(90, 20, 2, " ### ##### ### ##### ### ");
+	object cactus = obj_init(90, 20, 2, " ### ##### ### ##### ### ", 5, 5);
+	object player = obj_init(5, 20, 1, "     ##  #   ########## #######  #   #  ", 8, 5);
 
 	draw_the_object(cactus);
+	draw_the_object(player);
 
+	// To be removed
+	// Testing the movement, looks good, needs work, also don't forget about game loop
+	for (int i = 0; i < 100; i++) {
+		getchar();
+		system("cls");
+
+		player.x++;
+		draw_the_object(player);
+		draw_the_object(cactus);
+	}
 }
 
-// make a bigger _sprite array and add size parameters
-// or better try to find a way how to pass the array without doing any costly loop
-object obj_init(int _x, int _y, int _owner, char _sprite[26]) {
+// make a bigger _sprite array and add size parameters - done
+// or better try to find a way how to pass the array without doing any costly loop - still having to think about it
+object obj_init(int _x, int _y, int _owner, char _sprite[], int _sizeX, int _sizeY) {
 	object _obj;
 
 	_obj.x = _x;
 	_obj.y = _y;
+	_obj._lx = _sizeX;
+	_obj._ly = _sizeY;
 	_obj.owner = _owner;
 
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			_obj.sprite[i][j] = _sprite[i * 5 + j];
+	for (int i = 0; i < _sizeY; i++) {
+		for (int j = 0; j < _sizeX; j++) {
+			_obj.sprite[i][j] = _sprite[i * _sizeX + j];
 		}
 	}
 
@@ -57,13 +71,13 @@ void set_console_pos(int _x, int _y) {
 
 void draw_the_object(object _go) {
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < _go._ly; i++) {
 		set_console_pos(_go.x, _go.y + i);
 
-		for (int j = 0; j < 5; j++) {
+		for (int j = 0; j < _go._lx; j++) {
 			printf("%c", _go.sprite[i][j]);
 		}
 	}
 
-	set_console_pos(0, y++); y++;
+	set_console_pos(0, 30); //y++;
 }
